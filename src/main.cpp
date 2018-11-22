7dfd41583337fad962fec63d8e5601d5bc29488d
Arimichi Matsumura <12750219+popon-01@users.noreply.github.com>
2018-11-22 16:35:08 +0900

    ImpleUDment UDP communicaction with client

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,80 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
+
+#include "password.h"
+
+const char* ssid = AP_SSID; //  your network SSID (name)
+const char* password = AP_PASSWORD;    // your network password (use for WPA, or use as key for WEP)
+
+int status = WL_IDLE_STATUS;
+int udp_port = 3333;
+WiFiUDP UDP;
+
+char WiFibuff[128];
+
+void printWifiStatus();
 
 void setup() {
-  // put your setup code here, to run once:
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  /*
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue:
+    while (true);
+  }
+
+  String fv = WiFi.FirmwareVersion();
+  if (fv != "1.1.0") {
+    Serial.println("Please upgrade the firmware");
+  }
+  */
+
+  // attempt to connect to Wifi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    status = WiFi.begin(ssid, password);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+
+  UDP.begin(udp_port);   
+  // you're connected now, so print out the status:
+  printWifiStatus();
 }
 
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(UDP.parsePacket() > 0) {
+    UDP.read(WiFibuff, 128);
+    Serial.print(WiFibuff);
+    UDP.flush();
+  }
+  delay(500);
+}
+
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
 }
