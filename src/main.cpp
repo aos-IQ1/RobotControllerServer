d5f9ce9fb49cf1a12467752351cf1f3e0faab5b5
chakku <chakkurai0724@outlook.jp>
2018-11-29 15:48:19 +0900

    送信を実装

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,16 @@ int udp_port = 3333;
 WiFiUDP UDP;
 
 char WiFibuff[128];
-bool command_send_success;  // コマンドのackが成功を示していればtrue,それ以外ならばfalse
+
+// コマンドの送信に成功したかどうかのAck
+bool command_send_success = false;
+
+// 命令列
+uint8_t stop_motion_command[] = {0x09,0x00,0x02,0x00,0x00,0x00,0x00,0x00,0x0b};
+uint8_t reset_program_counter_command[] = {0x11,0x00,0x02,0x02,0x00,0x00,0x4B,0x04,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x64};
+uint8_t address_command[] = {0x07,0x0C,0x80,0x0B,0x00,0x00,0x9E};
+uint8_t motion_restart_command[] = {0x09,0x00,0x02,0x00,0x00,0x00,0x03,0x00,0x0E};
+
 
 void printWifiStatus();
 void connect_WiFi();
@@ -20,6 +29,7 @@ void wait_command_ack();
 
 void setup() {
   Serial.begin(115200);
+  Serial2.begin(115200,SERIAL_8E1,2,2,false);
   M5.begin();
   M5.Lcd.println("Start RobotControllerServer");
 
@@ -33,6 +43,7 @@ void setup() {
 
 
 void loop(){ 
+  /*
   if(UDP.parsePacket() > 0) {
     UDP.read(WiFibuff, 128);
     Serial.print(WiFibuff);
@@ -40,7 +51,27 @@ void loop(){
   }else{
     M5.Lcd.println("NO UDP");
   }
+  */
 
+
+  // シリアル通信で命令列をロボットに送信
+  Serial2.write(stop_motion_command,stop_motion_command[0]);
+  wait_command_ack();
+  if(!command_send_success) return;
+
+  Serial2.write(reset_program_counter_command,reset_program_counter_command[0]);
+  wait_command_ack();
+  if(!command_send_success) return;
+
+  Serial2.write(address_command,address_command[0]);
+  wait_command_ack();
+  if(!command_send_success) return;
+
+  Serial2.write(motion_restart_command,motion_restart_command[0]);
+  wait_command_ack();
+  if(!command_send_success) return;
+  
+  //ロボットからのモーション完了を待つ
 
   delay(500);
 }
@@ -82,14 +113,13 @@ void connect_WiFi(){
   @return 返り値が成功ならばtrue,失敗ならばfalse
 */
 void wait_command_ack(){
-  while(Serial.available() == 0){}
+  while(Serial2.available() == 0){}
 
-  uint8_t command_length = Serial.read();
+  uint8_t command_length = Serial2.read();
   uint8_t commands[10];
   for(uint8_t i=1; i<command_length; i++){
-    commands[i] = Serial.read();
+    commands[i] = Serial2.read();
   }
-
 
   command_send_success = (commands[2] == 6);
 }
